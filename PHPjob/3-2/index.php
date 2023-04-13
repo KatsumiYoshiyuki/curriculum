03412ab3d60ba339548b95e44ea8bc8117551667
KatsumiYoshiyuki <129579465+KatsumiYoshiyuki@users.noreply.github.com>
2023-04-13 13:43:00 +0900

    3章　チェックテスト
    再レビュー

--- PHPjob/3-2/index.php
+++ PHPjob/3-2/index.php
@@ -1,28 +1,21 @@
-<html>
-    <?php
-        //商品の税込価格を計算
-        //①税率を定数TAXに格納
-        define('TAX',1.1);
+<?php
+    //商品の税込価格を計算
+    //①税率を定数TAXに格納
+    define('TAX',1.1);
 
-        //各価格を変数に格納
-        $pencil = 100;
-        $eraser = 150;
-        $protractor = 500;
+    //②商品の情報を連想配列に格納
+    $products = array("鉛筆" => 100, "消しゴム" => 150, "物差し" => 500);
+    foreach($products as $key => $value){
 
-        //②商品の情報を連想配列に格納
-        $products = array("鉛筆" => $pencil, "消しゴム" => $eraser, "物差し" => $protractor);
-        foreach($products as $key => $value){
+        //③税込み価格計算処理
+        $taxval = calcValue($value);
+        echo $key."の税込み価格は".$taxval."円です"."<br>";
+    }
+    
 
-            //③税込み価格計算処理
-            $taxval = calcValue($value);
-            echo $key."の税込み価格は".$taxval."円です"."<br>";
-        }
-        
-
-        function calcValue($value){
-            //税込価格計算
-            $taxval= $value * TAX;
-            return $taxval;
-        }
-    ?>
-</html>
+    function calcValue($value){
+        //税込価格計算
+        $taxval= $value * TAX;
+        return $taxval;
+    }
+?>
