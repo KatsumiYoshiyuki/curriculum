206409a655caf9e563788fc00f52e0b44834c367
KatsumiYoshiyuki <129579465+KatsumiYoshiyuki@users.noreply.github.com>
2023-04-13 13:43:42 +0900

    Revert "3章　チェックテスト"
    This reverts commit 03412ab3d60ba339548b95e44ea8bc8117551667.

--- PHPjob/3-2/index.php
+++ PHPjob/3-2/index.php
@@ -1,21 +1,28 @@
-<?php
-    //商品の税込価格を計算
-    //①税率を定数TAXに格納
-    define('TAX',1.1);
+<html>
+    <?php
+        //商品の税込価格を計算
+        //①税率を定数TAXに格納
+        define('TAX',1.1);
 
-    //②商品の情報を連想配列に格納
-    $products = array("鉛筆" => 100, "消しゴム" => 150, "物差し" => 500);
-    foreach($products as $key => $value){
+        //各価格を変数に格納
+        $pencil = 100;
+        $eraser = 150;
+        $protractor = 500;
 
-        //③税込み価格計算処理
-        $taxval = calcValue($value);
-        echo $key."の税込み価格は".$taxval."円です"."<br>";
-    }
-    
+        //②商品の情報を連想配列に格納
+        $products = array("鉛筆" => $pencil, "消しゴム" => $eraser, "物差し" => $protractor);
+        foreach($products as $key => $value){
 
-    function calcValue($value){
-        //税込価格計算
-        $taxval= $value * TAX;
-        return $taxval;
-    }
-?>
+            //③税込み価格計算処理
+            $taxval = calcValue($value);
+            echo $key."の税込み価格は".$taxval."円です"."<br>";
+        }
+        
+
+        function calcValue($value){
+            //税込価格計算
+            $taxval= $value * TAX;
+            return $taxval;
+        }
+    ?>
+</html>
